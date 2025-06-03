9c05dba7ead1f661a7fad8bf2705faccea2caeb8
onur846 <115958771+onur846@users.noreply.github.com>
2025-06-03 05:28:48 +0300

    Update top25.js

--- pages/api/top25.js
+++ pages/api/top25.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://loud-proxy.onrender.com/top25');
+    const response = await fetch('https://loud-proxy.onrender.com');
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
