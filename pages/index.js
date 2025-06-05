dfb855c316ba747f74cca93c9b5251a6c4ba71e4
onur846 <115958771+onur846@users.noreply.github.com>
2025-06-06 00:19:32 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,10 @@ export default function Home() {
         <title>LOUD Top 25 Analyzer</title>
       </Head>
 
+    <a href="/" className="logo-link">
+      <img src="/loudion.png" alt="Loudions Logo" className="loudion-logo" />
+    </a>
+
       <header className="header">
         <h1>🚀 LOUD Top 25 Analyzer</h1>
         <p>Discover what the top $LOUD influencers are doing to stay ahead.</p>
